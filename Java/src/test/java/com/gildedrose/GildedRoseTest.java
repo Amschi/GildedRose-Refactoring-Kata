2876b83ca69a43967132933ba626dcd3c2af818a
Amaury Schillio <amaury.schillio@fabernovel.com>
2018-08-10 16:49:59 +0200

    add test before refactoring

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,12 +6,40 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
+
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void should_decrease_quality_by_two() {
+        Item[] items = new Item[]{
+          new Item("Aged", 0, 2)
+        };
+
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(0, app.items[0].quality);
     }
 
+    @Test
+    public void should_decrease_quality_to_0_and_selling_to_minus_1() {
+        Item[] items = new Item[]{
+          new Item("Backstage passes to a TAFKAL80ETC concert", 0, 40)
+        };
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
+
+    @Test
+    public void should_increase_quality_by_two_and_decrease_selin_by_one() {
+        Item[] items = new Item[]{
+          new Item("Aged Brie", -10, 40)
+        };
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(42, app.items[0].quality);
+        assertEquals(-11, app.items[0].sellIn);
+    }
 }
